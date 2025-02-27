85e61797bb8690c467fa83eb98b83f7cd7cf06ae
javierpomachagua <javierpp0303@gmail.com>
2025-02-27 16:38:30 -0500

    chore: initial setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+
+        $this->configureModels();
+
+        $this->configureVite();
+    }
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction()
+        );
+    }
+
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict();
+
+        Model::unguard();
+    }
+
+    private function configureVite(): void
+    {
+        Vite::usePrefetchStrategy('aggressive');
     }
 }
